2569598fe4615f764f863fbe3cb69b4776cf650a
IdioticMuthu <iammuthuraman2409@gmail.com>
2017-04-29 11:45:37 +0530

    Add files via upload

---
+++ ptveorntve.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{ int number;
+  scanf("%d",&number);
+  if(number==0)
+	printf("zero");
+  else if(number>0)
+	printf("positive");
+  else
+	printf("negative");
+  return 0;
+}
